1286420ccd5349d3fe09c6b5127b28b3649df01e
ali <amadouali0120@gmail.com>
2025-12-27 17:03:51 +0000

    Ajout des classes BandAdmin et ListingAdmin qui permettent de formater ou de definir les nom de champs qui doivent figurer dans notre espaces admin

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from listings.models import Band
+from listings.models import Listing
+
+class BandAdmin(admin.ModelAdmin): #nous inserons ces deux lignes 
+    """
+        liste les champs que nous voulons 
+        sur l'affichage de la liste
+    """
+    list_display = ('name', 'year_formed', 'genre')
+     
+class ListingAdmin(admin.ModelAdmin):
+    
+    list_display = ('title', 'band', 'type')
+    
+admin.site.register(Band, BandAdmin)
+admin.site.register(Listing, ListingAdmin)
+
